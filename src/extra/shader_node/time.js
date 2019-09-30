18c1aa35fbed4e04e47d7cea376427ec62c6c635
wakewakame <0214.taiki@gmail.com>
2019-09-30 18:54:50 +0900

    fixed the method of getting time

--- src/extra/shader_node/time.js
+++ src/extra/shader_node/time.js
@@ -4,15 +4,18 @@ import { ValueNodeParam } from "./param.js";
 export const TimeNode = class extends Node {
 	constructor(x, y) {
 		super("float", "time", x, y);
-		this.outputFloatNodeParam = null;	}
+		this.outputFloatNodeParam = null;
+		this.time = 0;
+	}
 	setup(){
 		super.setup();
 		this.outputFloatNodeParam = this.outputs.add(new ValueNodeParam("float", "output"));
 		this.outputFloatNodeParam.value.x = 0.0;
+		this.time = Date.now();
 	}
 	job(){
 		super.job();
-		this.outputFloatNodeParam.value.x += 0.01;
+		this.outputFloatNodeParam.value.x = (Date.now() - this.time) / 1000.0;
 	}
 	update(){
 		super.update();
